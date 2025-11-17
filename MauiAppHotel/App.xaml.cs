2cca152a979801eab03f1f1e8bafe8cc0a171446
Juliano Ramalho <juliano_ap_ramalho@hotmail.com>
2025-11-17 00:51:12 -0300

    Adiciona suporte à seleção de suítes e detalhes da estadia
    - Adicionada lista de suítes na classe `App` com descrições e valores.
    - Adicionada cor `Gray700` e `Gray700Brush` em `Colors.xaml`.
    - Atualizado `ContratatacaoHospedagem.xaml` para exibir suítes no `Picker` e configurar eventos para check-in e botão "Avançar".
    - Configurado `ContratatacaoHospedagem.xaml.cs` para gerenciar datas e navegação.
    - Adicionada exibição de detalhes da hospedagem em `HospedagemContratada.xaml`.
    - Implementado botão "Voltar" em `HospedagemContratada.xaml.cs`.
    - Criada classe `Quarto` para representar suítes com descrição e valores.

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,9 +1,19 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
-        public App()
+		public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto { Descricao = "Suíte Super Luxo", ValorDiariaAdulto = 110.00, ValorDiariaCrianca = 55.00 },
+            new Quarto { Descricao = "Suíte Luxo", ValorDiariaAdulto = 80.00, ValorDiariaCrianca = 40.00 },
+            new Quarto { Descricao = "Suíte Single", ValorDiariaAdulto = 50.00, ValorDiariaCrianca = 30.00 }
+        };
+        		
+
+		public App()
         {
             InitializeComponent();
 
